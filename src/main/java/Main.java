f4f22e6f776c004143dd0aaa2a1559c9aaf84dcb
tianlinsy <tianlinsy2026@student.cis.edu.hk>
2024-03-05 12:12:24 +0800

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,36 @@
+import static java.awt.AWTEventMulticaster.add;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // call the first add method and store the result into a variable called resultOne.
+            int numOne = 5;
+            int numTwo = 7;
+            int resultOne = numOne + numTwo;
 
         // print resultOne.
+        System.out.println(resultOne);
 
         // call the greeting method and store the result into a variable called resultTwo.
+        String name = "John";
+        String resultTwo = greeting(name);
 
         // print resultTwo.
+        System.out.println(resultTwo);
 
         // call the second add method and store the result into a variable called resultThree.
+            int numThree = 6;
+            int numFour = 8;
+            int resultThree = add(numOne, numTwo, numThree, numFour);
 
         // print resultThree.
-
+        System.out.println(resultThree);
         // call the printCapitalised method.
+
+            String toBePrinted = "hello, world!";
+            printCapitalised(toBePrinted);
+
 
     }
 
@@ -26,11 +42,19 @@ public class Main {
      */
 
 
+        public static int add(int numOne, int numTwo) {
+            return numOne + numTwo;
+        }
+
     /**
      * greeting
      * @param name - the name of a person (a String)
      * @return a greeting in the format of "Bonjour, [name]!" (a String)
      */
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+            }
+
 
 
     /**
@@ -43,11 +67,23 @@ public class Main {
      */
 
 
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        int sumOne = add(numOne, numTwo);
+        int sumTwo = add(sumOne, numThree);
+        int sumThree = add(sumTwo, numFour);
+        return sumThree;
+    }
+
+
     /**
      * printCapitalised
      * @param toBePrinted - a String that the user wishes to be printed in all caps.
      */
 
+            public static void printCapitalised(String toBePrinted) {
+                String capitalised = toBePrinted.toUpperCase();
+                System.out.println(toBePrinted);
+    }
 
 
 
